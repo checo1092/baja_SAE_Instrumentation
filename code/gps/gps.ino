a6f3978fc644cfd2c67637ff8e72c1bedc6c2910
SergioDM <87045016+Sergio1092@users.noreply.github.com>
2023-07-11 13:51:51 -0600

    GPS code added.
    Test code for the GPS was added

---
+++ code/gps/gps.ino
@@ -0,0 +1,49 @@
+#include <SPI.h>
+#include <TinyGPS++.h>                       
+
+TinyGPSPlus gps;                            
+// HardwareSerial Serial1(1); // Glacierjay had to comment this out to remove linker warning
+
+int pktCount = 0; // count how many packets sent
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial1.begin(9600, SERIAL_8N1, 34, 12);   //17-TX 18-RX for GPS
+}
+
+void print_info_json(){
+  Serial.print("{\'Valid\': \'");
+  Serial.print(gps.location.isValid());
+  Serial.print("\', \'Lat\': \'");
+  Serial.print(gps.location.lat(), 5);
+  Serial.print("\', \'Long\': \'");
+  Serial.print(gps.location.lng(), 4);
+  Serial.print("\', \'Satellites\': \'");
+  Serial.print(gps.satellites.value());
+  Serial.print("\', \'Altitude\': \'");
+  Serial.print(gps.altitude.feet());
+  Serial.print("\', \'Time\': \'");
+  Serial.printf("%.2d:%.2d:%.2d",gps.time.hour(),gps.time.minute(),gps.time.second());
+  Serial.println("\'}");
+}
+
+void loop()
+{
+  print_info_json();
+
+  smartDelay(10000);                                      
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+    Serial.println(F("No GPS data received: check wiring"));
+}
+
+static void smartDelay(unsigned long ms)                
+{
+  unsigned long start = millis();
+  do
+  {
+    while (Serial1.available())
+      gps.encode(Serial1.read());
+  } while (millis() - start < ms);
+}
--- code/readme.md
+++ code/readme.md
@@ -7,3 +7,10 @@
 
 ***
 
+## Instalation
+
+## Instalation of the ESP32 board into the arduino IDE.
+
+1. Install the boad adding https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json to the additional board manager URLs field.	
+2. In the board manager search ESP32 and install ESP32 by Espressif Systems.
+
