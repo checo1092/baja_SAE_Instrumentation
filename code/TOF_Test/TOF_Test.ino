1dcc93301f6ccc3db442a176613257316d8eb25d
checo1092 <checo_1092@hotmail.com>
2023-07-19 21:50:02 -0600

    Update TOF_Test.ino

--- code/TOF_Test/TOF_Test.ino
+++ code/TOF_Test/TOF_Test.ino
@@ -1,120 +1,66 @@
-//Libraries for LoRa
-#include <SPI.h>
-#include <LoRa.h>
-
-//Libraries for OLED Display
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include "Adafruit_VL6180X.h"
 
-//define the pins used by the LoRa transceiver module
-#define SCK 5
-#define MISO 19
-#define MOSI 27
-#define SS 18
-#define RST 23
-#define DIO0 26
+Adafruit_VL6180X vl = Adafruit_VL6180X();
 
-// Parameters for the LoRa protocol
-#define BAND 433E6
-// Supported values are between 2 and 17, defualt to 14
-#define TXPOWER 17
-//Supported values are between 6 and 12, default to 7. 6 implies implicit header
-#define SPREDINGFACTOR 6
-// true enables implicit header mode, false enables explicit header mode (default)
-#define IMPLICIDHEADER true
-
-//OLED pins
-#define OLED_SDA 21
-#define OLED_SCL 22 
-#define OLED_RST 23
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RST);
-
-String LoRaData;
-//packet counter
-int counter = 0;
-
-void setup() { 
-  //initialize Serial Monitor
+void setup() {
   Serial.begin(115200);
-  
-  // //reset OLED display via software
-  // pinMode(OLED_RST, OUTPUT);
-  // digitalWrite(OLED_RST, LOW);
-  // delay(20);
-  // digitalWrite(OLED_RST, HIGH);
-  
-  //initialize OLED
-  Wire.begin(OLED_SDA, OLED_SCL);
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3c, false, false)) { // Address 0x3C for 128x32
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+
+  // wait for serial port to open on native usb devices
+  while (!Serial) {
+    delay(1);
   }
-
-  display.clearDisplay();
-  display.setTextColor(WHITE);
-  display.setTextSize(1);
-  display.setCursor(0,0);
-  display.print("LORA RECEIVER ");
-  display.display();
-
-  Serial.println("LoRa Receiver Test");
   
-  //SPI LoRa pins
-  SPI.begin(SCK, MISO, MOSI, SS);
-  //setup LoRa transceiver module
-  LoRa.setPins(SS, RST, DIO0);
-
-  if (!LoRa.begin(BAND)) {
-    Serial.println("Starting LoRa failed!");
+  Serial.println("Adafruit VL6180x test!");
+  if (! vl.begin()) {
+    Serial.println("Failed to find sensor");
     while (1);
   }
-  Serial.println("LoRa Initializing OK!");
-  display.setCursor(0,10);
-  display.println("LoRa Initializing OK!");
-  display.display();  
+  Serial.println("Sensor found!");
 }
 
 void loop() {
+  float lux = vl.readLux(VL6180X_ALS_GAIN_5);
 
-  //try to parse packet
-  int packetSize = LoRa.parsePacket();
-  if (packetSize) {
-    //received a packet
-    Serial.print("Received packet ");
-    counter ++;
+  //Serial.print("Lux: "); 
+  //Serial.println(lux);
+  
+  uint8_t range = vl.readRange();
+  uint8_t status = vl.readRangeStatus();
 
-    //read packet
-    while (LoRa.available()) {
-      LoRaData = LoRa.readString();
-      Serial.print(LoRaData);
-      Serial.print(", sended: ");
-      Serial.print(counter);
-    }
-
-    //print RSSI of packet
-    int rssi = LoRa.packetRssi();
-    Serial.print(" with RSSI ");    
-    Serial.println(rssi);
-
-   // Dsiplay information
-   display.clearDisplay();
-   display.setCursor(0,0);
-   display.print("LORA RECEIVER");
-   display.setCursor(0,20);
-   display.print("Received packet:");
-   display.setCursor(0,30);
-   display.print(LoRaData);
-  display.print(" of: ");
-   display.print(counter);
-
-   display.setCursor(0,40);
-   display.print("RSSI:");
-   display.setCursor(30,40);
-   display.print(rssi);
-   display.display();   
+  if (status == VL6180X_ERROR_NONE) {
+    //Serial.print("Range: "); 
+    Serial.println(range);
   }
+
+  // Some error occurred, print it out!
+  
+  if  ((status >= VL6180X_ERROR_SYSERR_1) && (status <= VL6180X_ERROR_SYSERR_5)) {
+    Serial.println("System error");
+  }
+  else if (status == VL6180X_ERROR_ECEFAIL) {
+    Serial.println("ECE failure");
+  }
+  else if (status == VL6180X_ERROR_NOCONVERGE) {
+    Serial.println("No convergence");
+  }
+  else if (status == VL6180X_ERROR_RANGEIGNORE) {
+    Serial.println("Ignoring range");
+  }
+  else if (status == VL6180X_ERROR_SNR) {
+    Serial.println("Signal/Noise error");
+  }
+  else if (status == VL6180X_ERROR_RAWUFLOW) {
+    Serial.println("Raw reading underflow");
+  }
+  else if (status == VL6180X_ERROR_RAWOFLOW) {
+    Serial.println("Raw reading overflow");
+  }
+  else if (status == VL6180X_ERROR_RANGEUFLOW) {
+    Serial.println("Range reading underflow");
+  }
+  else if (status == VL6180X_ERROR_RANGEOFLOW) {
+    Serial.println("Range reading overflow");
+  }
+  delay(100);
 }
