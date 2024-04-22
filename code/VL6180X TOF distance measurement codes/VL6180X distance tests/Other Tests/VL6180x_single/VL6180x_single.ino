39c4816ff30f91fdbb43bc61ca47ec9819a2d973
SIHIRION <ferdan.rc1997@gmail.com>
2024-04-22 16:36:07 -0600

    4 VL6180X distance sensor measurements
    Added:
     - Code for 4 VL6180X distance sensor measurements and more codes.
     - Organized codes in folders with more descriptive names
     - Update README

---
+++ code/VL6180X TOF distance measurement codes/VL6180X distance tests/Other Tests/VL6180x_single/VL6180x_single.ino
@@ -0,0 +1,105 @@
+/*
+ *  Arduino code 
+Using single VL6180X 20cm Time-of-Flight proximity sensor with Arduino
+
+View code for using single VL6180X sensors: https://robojax.com/learn/arduino/?vid=robojax_VL6180X_laser2
+
+ * Original code and library by https://github.com/adafruit/Adafruit_VL6180X
+ * 
+ * Written/updated by Ahmad Shamshiri for Robojax Robojax.com
+ * on Mar 10, 2021  in Ajax, Ontario, Canada
+ Watch the video instruction for this sketch: https://youtu.be/_H9D0czQpSI
+ 
+
+
+If you found this tutorial helpful, please support me so I can continue creating 
+content like this. You can support me on Patreon http://robojax.com/L/?id=63
+or make donation using PayPal http://robojax.com/L/?id=64
+
+* 
+ * Code is available at http://robojax.com/learn/arduino
+
+ * This code is "AS IS" without warranty or liability. Free to be used as long as you keep this note intact.* 
+ * This code has been download from Robojax.com
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+
+*/
+#include <Wire.h>
+#include "Adafruit_VL6180X.h"
+
+Adafruit_VL6180X vl = Adafruit_VL6180X();
+
+void setup() {
+  Serial.begin(115200);
+
+  // wait for serial port to open on native usb devices
+  while (!Serial) {
+    delay(1);
+  }
+  
+  Serial.println("Adafruit VL6180x test!");
+  if (! vl.begin()) {
+    Serial.println("Failed to find sensor");
+    while (1);
+  }
+  Serial.println("Sensor found!");
+}
+
+void loop() {
+
+  uint8_t range = vl.readRange();
+  uint8_t status = vl.readRangeStatus();
+
+  if (status == VL6180X_ERROR_NONE) {
+    //Serial.print("Range: "); 
+    Serial.println(range);
+    //Serial.println("mm");
+  }
+//  if(range <=76)
+//  {
+//    //do something
+//  }
+
+  // Some error occurred, print it out!
+  
+  if  ((status >= VL6180X_ERROR_SYSERR_1) && (status <= VL6180X_ERROR_SYSERR_5)) {
+    Serial.println("System error");
+  }
+  else if (status == VL6180X_ERROR_ECEFAIL) {
+    Serial.println("ECE failure");
+  }
+  else if (status == VL6180X_ERROR_NOCONVERGE) {
+    Serial.println("No convergence");
+  }
+  else if (status == VL6180X_ERROR_RANGEIGNORE) {
+    Serial.println("Ignoring range");
+  }
+  else if (status == VL6180X_ERROR_SNR) {
+    Serial.println("Signal/Noise error");
+  }
+  else if (status == VL6180X_ERROR_RAWUFLOW) {
+    Serial.println("Raw reading underflow");
+  }
+  else if (status == VL6180X_ERROR_RAWOFLOW) {
+    Serial.println("Raw reading overflow");
+  }
+  else if (status == VL6180X_ERROR_RANGEUFLOW) {
+    Serial.println("Range reading underflow");
+  }
+  else if (status == VL6180X_ERROR_RANGEOFLOW) {
+    Serial.println("Range reading overflow");
+  }
+  delay(50);
+}
+ 
--- code/readme.md
+++ code/readme.md
@@ -3,16 +3,42 @@
 
 *** 
 ## Files
-1. /gps	Arduino code to test the gps sensor.
-2. /receiver	Arduino code for the receiver device.
-3. /sender	Arduino code for the sender device.
+1. /GPS measurement codes    ESP32WROOM with GY-GPSU3-NEO module for GPS measurements. 
+2. /LORA transmition codes   ESP32WROOM with E220-900T30D module for LORA information transmitions. 
+3. /I2C transmition codes    ESP32WROOM I2C communication. 
+4. /tpmsReading              
+5. /transmission_Test
 
 ***
 
 ## Instalation of the ESP32 board and libraries into the arduino IDE.
 1. Install the boad adding https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json to the additional board manager URLs field.	
 2. In the board manager search ESP32 and install ESP32 by Espressif Systems.
-3. Install the arduino-LoRa library by sandeep mistry. 
-4. Install the Adafruit SSD1306 library by Adafruit. (TTGO Dispaly version)
-5. Install the Adafruit GFX library by Adafruit. (TTGO Display version)
-6. Install the VL6180X library by Pololu. (TOF sensor)
+3. Install the "LoRa" library by sandeep mistry. 
+4. Install the "Adafruit SSD1306" library by Adafruit. (TTGO Dispaly version)
+5. Install the "Adafruit GFX Library" by Adafruit. (TTGO Display version)
+6. Install the "Adafruit_VL6180X library" by Adafruit. (VL6180X TOF distance sensor)
+7. Install the "ArduinoJson" library by Benoit Blanchon. 
+
+## Tested codes
+
+For distance measurements using the VL6180X TOF distance sensor, use "VL6180x_four" Arduino code. 
+
+## On development
+
+ - Create JSON data structure and send via I2C communication with other ESP32WROOM
+ - Test LoRa codes for sensor measurements remote monitoring. 
+
+## Notes on VL6180X TOF distance measurements
+
+It is necesary to modify Adafruit_VL6180X.cpp file. 
+
+Add these lines at the end of method Adafruit_VL6180X::loadSettings(void): 
+
+  write8(0x0024, 0x14); // SYSRANGE__PART_TO_PART_RANGE_OFFSET
+  write8(0x010a, 0x18); // READOUT__AVERAGING_SAMPLE_PERIOD
+  write8(0x001c, 0x05); //SYSRANGE__MAX_CONVERGENCE_TIME
+
+These lines access registers of VL6180X internal microcontroller. They modify measurement parameters for used mode "single range measurement", see datasheet for default values. 
+
+
